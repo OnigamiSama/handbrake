cd395295c30a2757859e064a37209550f942531d
OnigamiSama <contact@onigami.fr>
2018-01-23 17:11:12 +0100

    Create handbrake.sh

---
+++ handbrake.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+SRC=/data/mediaserver/Handbrake/RAW/
+DEST=/data/mediaserver/Handbrake/Post/
+DEST_EXT=mkv
+HANDBRAKE_CLI=HandBrakeCLI
+PRESET="H.265 MKV 1080p30"
+IFS=$(echo -en "\n\b")
+#
+#
+for FILE in `ls $SRC`
+do
+        filename=$(basename $FILE)
+        extension=${filename##*.}
+        filename=${filename%.*}
+#
+        $HANDBRAKE_CLI --all-audio --all-subtitles -i $SRC/$FILE -o $DEST/$filename.$DEST_EXT --preset="$PRESET"
+done
